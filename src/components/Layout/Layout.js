0252dddaf770b1e9c1aa4b9865dd2ff28da3ca6f
lucascprazeres <olucascostadp@gmail.com>
2020-05-22 21:00:23 -0300

    started the toolbar implementation

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,11 +2,12 @@ import React from 'react';
 import classes from './Layout.module.css';
 
 import Aux from '../../hoc/Aux/Aux';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const Layout = (props) => (
   <Aux>
     <div>
-      Toolbar, Sidedrawer, Backdrop
+      <Toolbar />
     </div>
     <main className={classes.Main}>
       {props.children}

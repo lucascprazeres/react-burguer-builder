265801d57b52dfe8b0881bc57d93afaa4e932b45
lucascprazeres <olucascostadp@gmail.com>
2020-05-23 19:15:26 -0300

    added an icon on the DrawerToggler

--- src/components/Navigation/SideDrawer/DrawerToggler/DrawerToggler.js
+++ src/components/Navigation/SideDrawer/DrawerToggler/DrawerToggler.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
+import classes from './DrawerToggler.module.css';
+
 const DrawerToggler = (props) => (
-  <div>
-    <button
-      type="button"
-      onClick={props.clicked}
-    >Toggle
-    </button>
+  <div className={classes.DrawerToggler} onClick={props.clicked}>
+    <div></div>
+    <div></div>
+    <div></div>
   </div>
 );
 

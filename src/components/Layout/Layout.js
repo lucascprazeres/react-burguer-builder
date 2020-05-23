3aba12504e219370667c6e3a1059ef7357335124
lucascprazeres <olucascostadp@gmail.com>
2020-05-23 17:38:05 -0300

    reused the Backdrop component on the sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Layout.module.css';
 
 import Aux from '../../hoc/Aux/Aux';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const Layout = (props) => (
-  <Aux>
-    <div>
-      <Toolbar />
-      <SideDrawer />
-    </div>
-    <main className={classes.Main}>
-      {props.children}
-    </main>
-  </Aux>
-);
+class Layout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showSideDrawer: true,
+    };
+  }
+
+  closedSideDrawerHandler = () => {
+    this.setState({ showSideDrawer: false });
+  }
+
+  render() {
+    return (
+      <Aux>
+        <div>
+          <Toolbar />
+          <SideDrawer
+            open={this.state.showSideDrawer}
+            closed={this.closedSideDrawerHandler}
+          />
+        </div>
+        <main className={classes.Main}>
+          {this.props.children}
+        </main>
+      </Aux>
+    );
+  }
+}
 
 export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -4,18 +4,30 @@ import classes from './SideDrawer.module.css';
 
 import Logo from '../../Logo/Logo';
 import NavItems from '../NavItems/NavItems';
+import Aux from '../../../hoc/Aux/Aux';
+import Backdrop from '../../../UI/Backdrop/Backdrop';
 
 const SideDrawer = (props) => {
-  //  ...
+  const attachedClasses = [classes.SideDrawer];
+
+  if (props.open) {
+    attachedClasses.push(classes.Open);
+  } else {
+    attachedClasses.push(classes.Close);
+  }
+
   return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo />
+    <Aux>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavItems />
+        </nav>
       </div>
-      <nav>
-        <NavItems />
-      </nav>
-    </div>
+    </Aux>
   );
 };
 

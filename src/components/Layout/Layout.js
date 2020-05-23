d8283e45ba3ccc5afb858d051aa37be925e13515
lucascprazeres <olucascostadp@gmail.com>
2020-05-23 15:54:29 -0300

    added a responsive SideDrawer (without adjusting it to the current layout)

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,11 +3,13 @@ import classes from './Layout.module.css';
 
 import Aux from '../../hoc/Aux/Aux';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const Layout = (props) => (
   <Aux>
     <div>
       <Toolbar />
+      <SideDrawer />
     </div>
     <main className={classes.Main}>
       {props.children}
---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import classes from './SideDrawer.module.css';
+
+import Logo from '../../Logo/Logo';
+import NavItems from '../NavItems/NavItems';
+
+const SideDrawer = (props) => {
+  //  ...
+  return (
+    <div className={classes.SideDrawer}>
+      <Logo />
+      <nav>
+        <NavItems />
+      </nav>
+    </div>
+  );
+};
+
+export default SideDrawer;

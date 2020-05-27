49d88e8694e9cc538f614365c11ae228d1dd20e4
lucascprazeres <olucascostadp@gmail.com>
2020-05-27 20:38:32 -0300

    implemented order post request to firebase

--- src/containers/BurgerBuilder/BurguerBuilder.js
+++ src/containers/BurgerBuilder/BurguerBuilder.js
@@ -6,6 +6,8 @@ import BuildControls from '../../components/Burguer/BuildControls/BuildControls'
 import Modal from '../../UI/Modal/Modal';
 import OrderSummary from '../../components/Burguer/OrderSummary/OrderSummary';
 
+import axios from '../../axios-orders';
+
 const INGREDIENT_PRICES = {
   salad: 0.5,
   cheese: 0.4,
@@ -73,7 +75,24 @@ class BurguerBuilder extends Component {
   }
 
   continuePurchaseHandler = () => {
-    alert('You continued!');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Lucas dos Prazeres',
+        address: {
+          street: 'testsreet 1',
+          zipCode: '123456',
+          country: 'Brazil',
+        },
+        email: 'email@example.com',
+      },
+      deliveryMethod: 'fastest',
+    };
+
+    axios.post('/orders.json', order)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
   }
 
   getEnabledButtons = () => {
